19da6dde85be62178a84f11900dc4e189265874d
dalismiles <dali.iapichino@gmail.com>
2022-09-28 23:16:51 +0200

    feat: explore init

--- src/pages/explore/explore.jsx
+++ src/pages/explore/explore.jsx
@@ -1,11 +1,17 @@
 import styles from "./index.module.scss";
-import {CardList} from "../../components/CardList"
+import { CardList } from "../../components/CardList";
 
 export function Explore() {
   return (
-      <div>
-        <CardList />  
-      </div>
-    
+    <div className={styles.Main}>
+      <span>CATEGORIA 1</span>
+      <div className={styles.CategoryList}>{/* <CardList />   */}</div>
+      <span>CATEGORIA 2</span>
+      <div className={styles.CategoryList}>{/* <CardList />   */}</div>
+      <span>CATEGORIA 3</span>
+      <div className={styles.CategoryList}>{/* <CardList />   */}</div>
+      <span>CATEGORIA 4</span>
+      <div className={styles.CategoryList}>{/* <CardList />   */}</div>
+    </div>
   );
 }
