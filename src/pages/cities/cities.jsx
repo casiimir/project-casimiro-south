7db8c60c21812330555f76b3e3f4a14d4404f300
Disti98 <antodiste118@gmail.com>
2022-09-30 22:10:46 +0200

    feat: populate cards city page

--- src/pages/cities/cities.jsx
+++ src/pages/cities/cities.jsx
@@ -1,37 +1,59 @@
 import { ENDPOINTS } from "../../utils/api/endpoints";
 import { useDispatch, useSelector } from "react-redux";
-
+import MainCard from "../../components/MainCard";
+import { CardList } from "../../components/CardList";
 import styles from "./index.module.scss";
+import { useEffect } from "react";
 
 export function Cities() {
-  const { listsData, lang, currency } = useSelector((state) => state);
+  const { listsData, lang, currency, cityData } = useSelector((state) => state);
+  const { id, name, content, headline, cover_img } = cityData;
 
   const dispatch = useDispatch();
   useEffect(() => {
-    fetch(ENDPOINTS.CITY_ACTIVITIES, {
-      method: "GET",
-      headers: {
-        "Accept-Language": lang.value,
-        "X-Musement-Application": "string",
-        "X-Musement-Version": "3.4.0",
-        "X-Musement-Currency": currency.value,
-      },
-    })
+    fetch(
+      `https://api.musement.com/api/v3/cities/${id}/activities?limit=100&offset=0`,
+      {
+        method: "GET",
+        headers: {
+          "Accept-Language": lang.value,
+          "X-Musement-Application": "string",
+          "X-Musement-Version": "3.4.0",
+          "X-Musement-Currency": currency.value,
+        },
+      }
+    )
       .then((response) => response.json())
       .then((json) =>
         dispatch({ type: "CITY_ACTIVITIES_LIST", payload: json })
       );
-  }, [lang.value, currency.value]);
+  }, [id, lang.value, currency.value]);
   const { datoP1, datoP2, datoP3 } = listsData.cityAct;
 
   return (
-    <div className={styles.Main}>
-      <div className={styles.ActivityList}>
-        {listsData.cityAct?.map((el, i) => (
-          <MainCard data={el} />
-        ))}
+    <div
+      className={styles.Main}
+      style={{
+        backgroundImage: `url(${cover_img}?w=1920)`,
+        objectFit: "cover",
+        backgroundSize: "100%",
+        backgroundRepeat: "no-repeat",
+        // backdropFilter: "blur(500px)",
+      }}
+    >
+      <div className={styles.hero}>
+        <div className={styles.hero_right}>
+          <h2 className={styles.headline}>{headline.replace(name, "")}</h2>
+          <h1 className={styles.name}>{name}</h1>
+        </div>
+        <div className={styles.hero.left}>form placeholder</div>
       </div>
+      <button onClick={() => console.log(cityData)}>console</button>
+      <CardList>
+        {listsData.cityAct?.map((el, i) => (
+          <MainCard data={el} key={i} />
+        ))}
+      </CardList>
     </div>
   );
 }
-
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,23 @@ import { combineReducers, createStore } from "redux";
 const init = {
   lang: { toggle: false, value: "en-US" },
   currency: { toggle: false, value: "USD" },
-  listsData: { home: [], expCity: [], expFD: [], expFolk: [], expNew: [] },
+  listsData: {
+    home: [],
+    expCity: [],
+    expFD: [],
+    expFolk: [],
+    expNew: [],
+    cityAct: [],
+  },
+  cityData: {
+    id: 189,
+    name: "Tokyo",
+    content:
+      "Tokyo is the capital city where blazing hyperactive neon lights meet the ancient Zen traditions that define Japanese culture.\nWith a population of almost 35.000 million people, it is considered one of the biggest metropolises on the planet. Perfectly balanced, where its heart has a technological pulse and its soul radiates its ancient past, travelers have an infinity of choices to choose from when visiting this city.\nIts traditional core lies in the Asakusa district, where old Buddhist temples and shrines echo Tokyo's spirituality. The district's iconic and most ancient building is the Senso-ji temple, visited daily by thousands of people. Visitors should also take a stroll down Nakamise Dori, a market street next to Asakusa, for souvenirs and delicious food.\nFor hip neighborhoods and chilled out vibes, travelers should head over to the Harajuku district and Yoyogi park to enjoy local laid-back experiences. Moving on to the more modern side of the city, the Tokyo Skytree, Tokyo Tower and Shibuya district are a must-see to embrace the city's modern atmosphere and high-level shopping.\nFor a more family and youth-oriented atmosphere, Tokyo Disneyland and the Akihabara Electric Town, an area packed with anime, video-gamed and neon themed activities are a must when visiting this city.\nIf travelers instead want to experience fun nights out, entertainment and dining then the Shinjuku neighborhood is perfect for these moments that will last a lifetime.\nLast but not least, Tokyo is one of the culinary capitals of the world, where you can virtually have any kind of Japanese dish, be it street-food or a more \"top-notch\" dining experience.",
+    headline: "Welcome to Tokyo",
+    cover_img:
+      "https://images.musement.com/cover/0097/13/adobestock-314643692-editorial-use-only-jpeg_header-9612533.jpeg",
+  },
 };
 
 function langReducer(state = {}, action) {
@@ -59,6 +75,43 @@ function listsReducer(state = {}, action) {
         ...state,
         expNew: action.payload,
       };
+    case "CITY_ACTIVITIES_LIST":
+      return {
+        ...state,
+        cityAct: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function cityDataReducer(state = {}, action) {
+  switch (action.type) {
+    case "SET_CITY_ID":
+      return {
+        ...state,
+        id: action.payload,
+      };
+    case "SET_CITY_NAME":
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case "SET_CITY_CONTENT":
+      return {
+        ...state,
+        content: action.payload,
+      };
+    case "SET_CITY_HEADLINE":
+      return {
+        ...state,
+        headline: action.payload,
+      };
+    case "SET_CITY_COVER_IMG":
+      return {
+        ...state,
+        cover_img: action.payload,
+      };
     default:
       return state;
   }
@@ -68,6 +121,7 @@ const rootReducers = combineReducers({
   lang: langReducer,
   currency: currencyReducer,
   listsData: listsReducer,
+  cityData: cityDataReducer,
 });
 
 const store = createStore(rootReducers, init);
