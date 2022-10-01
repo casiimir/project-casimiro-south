cfdae9b3e021e89c119d1e85b8888b8b9f74359c
dalismiles <dali.iapichino@gmail.com>
2022-10-01 20:08:17 +0200

    feat about :api + populate page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,39 @@
+import TMCard from "../../components/TMCard";
+import teamMembers from "./../../utils/api/teamMembers";
+
 import styles from "./index.module.scss";
 
 export function About() {
-  return <div className={styles.Main}>hellouu! I am the about page!</div>;
+  return (
+    <div className={styles.Main}>
+      <div className={styles.description}>
+        <div className={styles.rectangle}></div>
+        <div className={styles.text}>
+          <div className={styles.title}>
+            <span>MEET </span>
+            THE TEAM
+          </div>
+          <div className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+            ratione perspiciatis laboriosam eum doloremque dignissimos
+            perferendis quaerat excepturi labore illo quas accusantium neque
+            facilis culpa, dolorem, optio architecto quasi, ipsa aut quibusdam.
+            Dolor repudiandae vel placeat debitis at reiciendis asperiores illo
+            deserunt amet blanditiis dolore, odio ipsam ipsum saepe nam.Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Unde ratione
+            perspiciatis laboriosam eum doloremque dignissimos perferendis
+            quaerat excepturi labore illo quas accusantium neque facilis culpa,
+            dolorem, optio architecto quasi, ipsa aut quibusdam. Dolor
+            repudiandae vel placeat debitis at reiciendis asperiores illo
+            deserunt amet blanditiis dolore, odio ipsam ipsum saepe nam.
+          </div>
+        </div>
+      </div>
+      <div className={styles.teamMembers}>
+        {teamMembers.map((name) => (
+          <TMCard key={name.id} data={name}/>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/utils/api/teamMembers.js
@@ -0,0 +1,78 @@
+import AntoninoPic from "../images/team/antonino_d.jfif";
+import DomenicoPic from "../images/team/domenico_s.jfif";
+import RobertaPic from "../images/team/roberta_d.jfif";
+import LuciaPic from "../images/team/lucia_d.jfif";
+import GinevraPic from "../images/team/ginevra_v.jfif";
+import DalilaPic from "../images/team/dalila_i.jpg";
+
+const teamMembers = [
+  {
+    id: 1,
+    name: "Antonino",
+    surname: "Di Stefano",
+    descriptionIta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    descriptionEng:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    src: AntoninoPic,
+    linkedin: "https://www.linkedin.com/in/antonino-di-stefano/",
+  },
+  {
+    id: 2,
+    name: "Domenico",
+    surname: "Seminara",
+    descriptionIta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    descriptionEng:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    src: DomenicoPic,
+    linkedin: "https://www.linkedin.com/in/domenico-seminara-1a139a238/",
+  },
+  {
+    id: 3,
+    name: "Roberta",
+    surname: "D'Agostino",
+    descriptionIta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    descriptionEng:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    src: RobertaPic,
+    linkedin: "https://www.linkedin.com/in/roberta-d-agostino/",
+  },
+  {
+    id: 4,
+    name: "Lucia",
+    surname: "Di Fede",
+    descriptionIta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    descriptionEng:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    src: LuciaPic,
+    linkedin: "https://www.linkedin.com/in/luciadifede/",
+  },
+  {
+    id: 5,
+    name: "Ginevra",
+    surname: "Vitrano",
+    descriptionIta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    descriptionEng:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    src: GinevraPic,
+    linkedin: "https://www.linkedin.com/in/ginevravitrano/",
+  },
+  {
+    id: 6,
+    name: "Dalila",
+    surname: "Iapichino",
+    descriptionIta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    descriptionEng:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!",
+    src: DalilaPic,
+    // src: "../images/team/dalila_i.jpg",
+    linkedin: "https://www.linkedin.com/in/dalilaiapichino/",
+  },
+];
+
+export default teamMembers;
