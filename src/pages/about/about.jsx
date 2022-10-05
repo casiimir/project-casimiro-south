cb76862a3b2c6437de428bd7d870dc72e9765ac2
dalismiles <dali.iapichino@gmail.com>
2022-10-05 19:00:28 +0200

    feat thankyoupage: style+responsive+text+minor fix

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -10,28 +10,43 @@ export function About() {
         <div className={styles.rectangle}></div>
         <div className={styles.text}>
           <div className={styles.title}>
-            <span>MEET </span>
-            THE TEAM
+            <p>
+              <span>WE ARE </span> JAPVENTURE
+            </p>
           </div>
-          <div className={styles.description}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-            ratione perspiciatis laboriosam eum doloremque dignissimos
-            perferendis quaerat excepturi labore illo quas accusantium neque
-            facilis culpa, dolorem, optio architecto quasi, ipsa aut quibusdam.
-            Dolor repudiandae vel placeat debitis at reiciendis asperiores illo
-            deserunt amet blanditiis dolore, odio ipsam ipsum saepe nam.Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Unde ratione
-            perspiciatis laboriosam eum doloremque dignissimos perferendis
-            quaerat excepturi labore illo quas accusantium neque facilis culpa,
-            dolorem, optio architecto quasi, ipsa aut quibusdam. Dolor
-            repudiandae vel placeat debitis at reiciendis asperiores illo
-            deserunt amet blanditiis dolore, odio ipsam ipsum saepe nam.
+          <div className={styles.weAreSection}>
+            <div className={styles.section}>
+              <h5>How It All Started</h5>
+              <p>
+                Project South inc. was founded in 2022 by Casimiro Ciancimino, a
+                FrontEnd Developer with one big idea: select 6 sicilian
+                frontenders and let them realize a big project.
+              </p>
+            </div>
+            <div className={styles.section}>
+              <h5>The Team</h5>
+              <p>
+                We are Antonino, Domenico, Roberta, Lucia, Ginevra and Dalila.
+                We invented, studied, designed and realized "Japventure" : an
+                intuitive platform, where globetrotters can discover Japan and
+                its amenities.
+              </p>
+            </div>
+            <div className={styles.section}>
+              <h5>Core Values</h5>
+              <p>
+                "We live by our core values. That’s why we believe in working
+                together, bee-ing positive, and inspiring each other. Because we
+                only succeed when our whole team does."
+              </p>
+            </div>
           </div>
         </div>
       </div>
+
       <div className={styles.teamMembers}>
         {teamMembers.map((name) => (
-          <TMCard key={name.id} data={name}/>
+          <TMCard key={name.id} data={name} />
         ))}
       </div>
     </div>
