beba8afbc96aa0f1065f84757d989a58c90f08c3
dalismiles <dali.iapichino@gmail.com>
2022-10-05 20:05:00 +0200

    feat thankyoupage: translation

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,9 +1,11 @@
+import { useSelector } from "react-redux";
 import TMCard from "../../components/TMCard";
 import teamMembers from "./../../utils/api/teamMembers";
 
 import styles from "./index.module.scss";
 
 export function About() {
+  const { lang } = useSelector((state) => state);
   return (
     <div className={styles.Main}>
       <div className={styles.description}>
@@ -16,28 +18,29 @@ export function About() {
           </div>
           <div className={styles.weAreSection}>
             <div className={styles.section}>
-              <h5>How It All Started</h5>
+              <h5>
+                {lang.toggle ? "Come tutto è cominciato" : "How It All Started"}
+              </h5>
               <p>
-                Project South inc. was founded in 2022 by Casimiro Ciancimino, a
-                FrontEnd Developer with one big idea: select 6 sicilian
-                frontenders and let them realize a big project.
+                {lang.toggle
+                  ? "Project South inc. è stata fondata nel 2022 da Casimiro Ciancimino, un FrontEnd Developer con una grande idea: selezionare 6 Frontenders siciliani e far realizzare loro un grande progetto."
+                  : "Project South inc. was founded in 2022 by Casimiro Ciancimino, a FrontEnd Developer with one big idea: select 6 sicilian frontenders and let them realize a big project."}
               </p>
             </div>
             <div className={styles.section}>
-              <h5>The Team</h5>
+              <h5>{lang.toggle ? "Il Team" : "The Team"}</h5>
               <p>
-                We are Antonino, Domenico, Roberta, Lucia, Ginevra and Dalila.
-                We invented, studied, designed and realized "Japventure" : an
-                intuitive platform, where globetrotters can discover Japan and
-                its amenities.
+                {lang.toggle
+                  ? 'Siamo Antonino, Domenico, Roberta, Lucia, Ginevra e Dalila. Abbiamo inventato, studiato, progettato e realizzato "Japventure": una piattaforma intuitiva, dove i giramondo possono scoprire il Giappone e le sue bellezze.'
+                  : 'We are Antonino, Domenico, Roberta, Lucia, Ginevra and Dalila. We invented, studied, designed and realized "Japventure" : an intuitive platform, where globetrotters can discover Japan and its amenities.'}
               </p>
             </div>
             <div className={styles.section}>
-              <h5>Core Values</h5>
+              <h5>{lang.toggle ? "Valori Fondamentali" : "Core Values"}</h5>
               <p>
-                "We live by our core values. That’s why we believe in working
-                together, bee-ing positive, and inspiring each other. Because we
-                only succeed when our whole team does."
+                {lang.toggle
+                  ? "Viviamo secondo i nostri valori fondamentali. Ecco perché crediamo nel lavorare insieme, nell'essere positivi e nell'ispirarci a vicenda. Perché abbiamo successo solo quando tutto il nostro team ha successo."
+                  : "We live by our core values. That’s why we believe in working together, bee-ing positive, and inspiring each other. Because we only succeed when our whole team does."}
               </p>
             </div>
           </div>
